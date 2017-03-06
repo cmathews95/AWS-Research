412b76dd0a9fe243266bbc21e1ef435cc55a1df8
cmathews95 <mathews.christie@gmail.com>
2017-03-06 13:27:41 -0800

    Run Asyn-Server with python2.6

---
+++ server.py
@@ -0,0 +1,61 @@
+#Run with Python 2.6                                                                                                                                         
+import os
+import sys
+import socket
+import threading
+import MySQLdb
+import random
+
+#Get Unique Instance ID                                                                                                                                      
+ID = MySQLdb.escape_string(str(os.popen("ec2-metadata -i").read()).strip('\n').strip('instance-id:').strip(' '))
+ADDRESS = "10.0.0.107"
+print  ID
+
+#Connect to db                                                                                                                                               
+db = MySQLdb.connect(
+         host="metricstestdb.cw4l1lykzmxj.us-west-1.rds.amazonaws.com",
+         port=3306,
+         user="cs199admin",
+         passwd="cs199password",
+         db="metrics_test_db")
+
+cur = db.cursor()
+
+add_message = ("INSERT INTO messages "
+               "(instanceID, message) "
+               "VALUES (%s, %s)")
+
+def saveData(name, connection):
+    # Receive the data in small chunks and retransmit it                                                                                                     
+    data = MySQLdb.escape_string(connection.recv(200))
+    print >>sys.stderr, 'received "%s"' % data
+    data_message = (ID, data)
+    cur.execute(add_message, data_message)
+    #cur.execute("SELECT * FROM messages")                                                                                                                   
+    db.commit()
+    connection.sendall("Added: " + data)
+    connection.close()
+
+def Main():
+    #Create a tcp/ip socket                                                                                                                                  
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = (address, 8000)
+    print >>sys.stderr, 'starting up on %s port %s' % server_address
+    sock.bind(server_address)
+
+    # Listen for incoming connections                                                                                                                        
+    sock.listen(5)
+    print "Server Started."
+    while True:
+        # Wait for a connection                                                                                                                              
+        print >>sys.stderr, 'waiting for a connection'
+        connection, client_address = sock.accept()
+        print "client connected ip:<" + str(client_address) + ">"
+        t = threading.Thread(target=saveData, args=("thread", connection))
+        t.start()
+    db.commit()
+    cur.close()
+    sock.close()
+
+if __name__ == '__main__':
+    Main()
