7f27eb18058944d0b4df87aad21edda628f89be9
EC2 Default User <ec2-user@ip-10-0-0-107.us-west-1.compute.internal>
2017-03-08 18:39:07 +0000

    Fixed variable name bug

--- server.py
+++ server.py
@@ -39,7 +39,7 @@ def saveData(name, connection):
 def Main():
     #Create a tcp/ip socket                                                                                                                                  
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = (address, 8000)
+    server_address = (ADDRESS, 8000)
     print >>sys.stderr, 'starting up on %s port %s' % server_address
     sock.bind(server_address)
 
